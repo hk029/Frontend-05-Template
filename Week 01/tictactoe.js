f36e63fbfc2ec629783020db2a35d463814ba45c
黄锴 <huangkai1@corp.netease.com>
2020-10-09 21:19:11 +0800

    feat:  完成Week01-胜利判断

--- Week 01/tictactoe.js
+++ Week 01/tictactoe.js
@@ -4,6 +4,12 @@ const SYMBOL = {
     CIRCLE: 2,
 };
 
+const CLASS_MAP = {
+    0: 'empty',
+    1: 'cross',
+    2: 'circle',
+};
+
 const pattern = [
     [0, 1, 0],
     [0, 2, 0],
@@ -12,10 +18,44 @@ const pattern = [
 
 let current = SYMBOL.CROSS;
 
+/**
+* 判断数组中的坐标是否是当前落子
+* @param {*} arr 
+*/
+const equalCurrent = (arr) => arr.every(([x, y]) => pattern[x][y] === current);
+
+/**
+ * 判断是否有人获胜
+ */
+const isWin = () => {
+
+    // 判断每行
+    for (let i = 0; i < 3; i++) {
+        if (equalCurrent([[i, 0], [i, 1], [i, 2]])) {
+            return true;
+        }
+    }
+    // 判断每列
+    for (let j = 0; j < 3; j++) {
+        if (equalCurrent([[0, j], [1, j], [2, j]])) {
+            return true;
+        }
+    }
+    // 判断角线
+    if (equalCurrent([[0, 0], [1, 1], [2, 2]])) {
+        return true;
+    }
+    if (equalCurrent([[0, 2], [1, 1], [2, 0]])) {
+        return true;
+    }
+    return false;
+}
+
 const renderCell = (cell, i, j) => {
     const klass =
         cell === SYMBOL.CROSS ? 'cross' :
             cell === SYMBOL.CIRCLE ? 'circle' : '';
+    // 通过class变更落子的文字，通过data-poin记录对应的数据位置
     return `<span class="cell ${klass}" data-point="${i}-${j}"></span>`
 }
 
@@ -33,32 +73,54 @@ const render = (root) => {
     `;
 }
 
-const addEvent = (root) => {
-    window.addEventListener('click', (ev) => {
-        const { target } = ev;
-        // 说明点击的是cell
-        if (target.dataset.point) {
-            // 获取当前点击的坐标
-            const { dataset: { point } } = target;
-            const [x, y] = point.split('-')
-            // 把当前的区域设置成对应的落子符号
-            if(pattern[x][y] === SYMBOL.EMPTY) {
-                pattern[x][y] = current;
-                // 交换当前符号
-                current = SYMBOL.CROSS + SYMBOL.CIRCLE - current;
-            }
-            // 重新渲染
-            render(root);
-        }
-    })
+const toggleClass = (cell) => {
+    // 说明当前已经有落子了
+    if (cell.classList.length > 1) {
+        return false;
+    }
+    cell.classList.add(CLASS_MAP[current]);
+    return true;
 }
+
+const exchange = () => (current = SYMBOL.CROSS + SYMBOL.CIRCLE - current);
+
+
+/**
+ * 移动子
+ * @param {*} ev 
+ */
+const move = (ev) => {
+    const { target } = ev;
+    // 说明点击的是cell
+    if (target.dataset.point) {
+        // 获取当前点击的坐标
+        const { dataset: { point } } = target;
+        const [x, y] = point.split('-')
+        // 把当前的区域设置成对应的落子符号
+        // 通过添加class可以完成换字，避免重新渲染
+        if (toggleClass(target)) {
+            pattern[x][y] = current;
+            if (isWin()) {
+                alert(`${CLASS_MAP[current]}赢了`)
+                removeEvent();
+                return;
+            }
+            // 交换落子
+            exchange();
+        }
+    }
+}
+
+const addEvent = () => window.addEventListener('click', move);
+
+const removeEvent = () => window.removeEventListener('click', move);
+
 
 
 const main = () => {
     const root = document.querySelector('#root');
     render(root);
     addEvent(root);
-
 }
 
 window.onload = () => {
