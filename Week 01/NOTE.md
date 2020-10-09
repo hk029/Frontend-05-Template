da4585004ff06839ea2cd44652b593530f1bfd72
黄锴 <huangkai1@corp.netease.com>
2020-10-09 20:37:05 +0800

    feat:  完成Week01-棋盘事件

--- Week 01/NOTE.md
+++ Week 01/NOTE.md
@@ -1,1 +1,12 @@
 学习笔记
+
+
+## 交换两个值
+用满交换律的任意运算（+-*/)
+如果刚好是0，1也可以用取反运算
+
+```js
+ // 交换当前符号
+ current = SYMBOL.CROSS + SYMBOL.CIRCLE - current;
+ current = SYMBOL.CROSS * SYMBOL.CIRCLE / current;
+```
--- Week 01/tictactoe.js
+++ Week 01/tictactoe.js
@@ -1,6 +1,7 @@
 const SYMBOL = {
-    CROSS : 1,
-    CIRCLE : 2,
+    EMPTY: 0,
+    CROSS: 1,
+    CIRCLE: 2,
 };
 
 const pattern = [
@@ -9,21 +10,22 @@ const pattern = [
     [0, 0, 0],
 ];
 
+let current = SYMBOL.CROSS;
 
-const renderCell = cell => {
-    const klass = 
-    cell === SYMBOL.CROSS ? 'cross' : 
-    cell === SYMBOL.CIRCLE ? 'circle' : '';
-    return `<span class="cell ${klass}" ></span>`
+const renderCell = (cell, i, j) => {
+    const klass =
+        cell === SYMBOL.CROSS ? 'cross' :
+            cell === SYMBOL.CIRCLE ? 'circle' : '';
+    return `<span class="cell ${klass}" data-point="${i}-${j}"></span>`
 }
 
-const renderRow = row => {
-    const cells = row.map(p => renderCell(p)).join('');
+const renderRow = (row, rowIdx) => {
+    const cells = row.map((p, idx) => renderCell(p, rowIdx, idx)).join('');
     return `<div class="row" >${cells}</div>`
 }
 
 const render = (root) => {
-    const rows = pattern.map(row => renderRow(row)).join('');
+    const rows = pattern.map((row, idx) => renderRow(row, idx)).join('');
     root.innerHTML = `
         <div class="pattern">
             ${rows}
@@ -31,9 +33,32 @@ const render = (root) => {
     `;
 }
 
+const addEvent = (root) => {
+    window.addEventListener('click', (ev) => {
+        const { target } = ev;
+        // 说明点击的是cell
+        if (target.dataset.point) {
+            // 获取当前点击的坐标
+            const { dataset: { point } } = target;
+            const [x, y] = point.split('-')
+            // 把当前的区域设置成对应的落子符号
+            if(pattern[x][y] === SYMBOL.EMPTY) {
+                pattern[x][y] = current;
+                // 交换当前符号
+                current = SYMBOL.CROSS + SYMBOL.CIRCLE - current;
+            }
+            // 重新渲染
+            render(root);
+        }
+    })
+}
+
+
 const main = () => {
     const root = document.querySelector('#root');
     render(root);
+    addEvent(root);
+
 }
 
 window.onload = () => {
