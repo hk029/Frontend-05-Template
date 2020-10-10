7d82b1a18565f9b442151cf78c81428e3742d529
黄锴 <huangkai1@corp.netease.com>
2020-10-10 19:35:21 +0800

    fix: 完成Week01-棋盘胜负判断

--- Week 01/tictactoe.js
+++ Week 01/tictactoe.js
@@ -10,7 +10,7 @@ const CLASS_MAP = {
     2: 'circle',
 };
 
-const pattern = [
+const PATTERN = [
     [0, 1, 0],
     [0, 2, 0],
     [0, 0, 0],
@@ -18,39 +18,83 @@ const pattern = [
 
 let current = SYMBOL.CROSS;
 
+////////////////////////////////
+//
+//           游戏策略
+//
+////////////////////////////////
+
+
 /**
-* 判断数组中的坐标是否是当前落子
-* @param {*} arr 
-*/
-const equalCurrent = (arr) => arr.every(([x, y]) => pattern[x][y] === current);
+ * 判断数组中的坐标是否是当前落子
+ * @param {*} pattern 
+ * @param {*} color 
+ * @return {Function} Array => Boolean
+ */
+const equalCurrent = (pattern, color) => arr => arr.every(([x, y]) => pattern[x][y] === color);
 
 /**
  * 判断是否有人获胜
  */
-const isWin = () => {
-
+const isWin = (pattern, color) => {
+    const equal = equalCurrent(pattern, color);
     // 判断每行
     for (let i = 0; i < 3; i++) {
-        if (equalCurrent([[i, 0], [i, 1], [i, 2]])) {
+        if (equal([[i, 0], [i, 1], [i, 2]])) {
             return true;
         }
     }
     // 判断每列
     for (let j = 0; j < 3; j++) {
-        if (equalCurrent([[0, j], [1, j], [2, j]])) {
+        if (equal([[0, j], [1, j], [2, j]])) {
             return true;
         }
     }
     // 判断角线
-    if (equalCurrent([[0, 0], [1, 1], [2, 2]])) {
+    if (equal([[0, 0], [1, 1], [2, 2]])) {
         return true;
     }
-    if (equalCurrent([[0, 2], [1, 1], [2, 0]])) {
+    if (equal([[0, 2], [1, 1], [2, 0]])) {
         return true;
     }
     return false;
 }
 
+/**
+ * 检查当前棋盘内是否有可以取胜的位置
+ */
+const willWin = (pattern, color) => {
+    const clone = v => JSON.parse(JSON.stringify(v));
+    
+    let win = false;
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            const cell = pattern[i][j];
+            // 如果可以落子则尝试落子
+            if (cell === 0) {
+                // 复制一个棋盘，尝试落子
+                const newPattern = clone(pattern);
+                newPattern[i][j] = color;
+                win = isWin(newPattern, color)
+                if (win) return win;
+            }
+        }
+    }
+    return win;
+}
+
+////////////////////////////////
+//
+//           渲染相关
+//
+////////////////////////////////
+
+/**
+ * 渲染一个格子
+ * @param {*} cell 
+ * @param {*} i 
+ * @param {*} j 
+ */
 const renderCell = (cell, i, j) => {
     const klass =
         cell === SYMBOL.CROSS ? 'cross' :
@@ -59,13 +103,22 @@ const renderCell = (cell, i, j) => {
     return `<span class="cell ${klass}" data-point="${i}-${j}"></span>`
 }
 
+/**
+ * 渲染一行
+ * @param {*} row 
+ * @param {*} rowIdx 
+ */
 const renderRow = (row, rowIdx) => {
     const cells = row.map((p, idx) => renderCell(p, rowIdx, idx)).join('');
     return `<div class="row" >${cells}</div>`
 }
 
+/**
+ * 在root结点上渲染棋盘
+ * @param {*} root 
+ */
 const render = (root) => {
-    const rows = pattern.map((row, idx) => renderRow(row, idx)).join('');
+    const rows = PATTERN.map((row, idx) => renderRow(row, idx)).join('');
     root.innerHTML = `
         <div class="pattern">
             ${rows}
@@ -73,6 +126,10 @@ const render = (root) => {
     `;
 }
 
+/**
+ * 修改当前格子的颜色
+ * @param {*} cell 
+ */
 const toggleClass = (cell) => {
     // 说明当前已经有落子了
     if (cell.classList.length > 1) {
@@ -82,8 +139,18 @@ const toggleClass = (cell) => {
     return true;
 }
 
+/**
+ * 交换落子
+ */
 const exchange = () => (current = SYMBOL.CROSS + SYMBOL.CIRCLE - current);
 
+
+
+////////////////////////////////
+//
+//           事件相关 
+//
+////////////////////////////////
 
 /**
  * 移动子
@@ -99,14 +166,18 @@ const move = (ev) => {
         // 把当前的区域设置成对应的落子符号
         // 通过添加class可以完成换字，避免重新渲染
         if (toggleClass(target)) {
-            pattern[x][y] = current;
-            if (isWin()) {
+            PATTERN[x][y] = current;
+            if (isWin(PATTERN, current)) {
                 alert(`${CLASS_MAP[current]}赢了`)
                 removeEvent();
                 return;
             }
             // 交换落子
             exchange();
+            // 判交换后是否可以胜利
+            if(willWin(PATTERN, current)){
+                console.log(`${CLASS_MAP[current]}将要赢了`)
+            }
         }
     }
 }
